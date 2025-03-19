9a75adf6b7a6696e72b87fad3565ae30f53b2096
mon1t0r <westxlebyshek@gmail.com>
2025-03-19 21:22:01 +0100

    Refactor

--- main.c
+++ main.c
@@ -13,34 +13,50 @@
 #include <linux/udp.h>
 #include "nat_table.h"
 
-/* Max IP packet size is 65535 bytes */
-#define BUF_SIZE 65536
-#define NAT_TABLE_SIZE 200
+#define ENABLE_LOG_INFO
+/*#define ENABLE_LOG_DEBUG*/
 
-#define NAT_PORT_RANGE_START 49160
-#define NAT_PORT_RANGE_END 50160
+#ifdef ENABLE_LOG_INFO
+#define LOG_INFO(...) printf(__VA_ARGS__)
+#else
+#define LOG_INFO(format, ...)
+#endif
 
-#define INT_NAME "eno1"
+#ifdef ENABLE_LOG_DEBUG
+#define LOG_DEBUG(...) printf(__VA_ARGS__)
+#else
+#define LOG_DEBUG(format, ...)
+#endif
 
-#define LISTEN_PORT 52880
+#define INTERFACE_NAME "eno1"
 
-#define GATEWAY_MAC_1 0x74
-#define GATEWAY_MAC_2 0xFE
-#define GATEWAY_MAC_3 0xCE
-#define GATEWAY_MAC_4 0x8C
-#define GATEWAY_MAC_5 0x87
-#define GATEWAY_MAC_6 0x11
-#define DEST_IP_1 146
-#define DEST_IP_2 190
-#define DEST_IP_3 62
-#define DEST_IP_4 39
-#define DEST_PORT 80
+enum {
+    packet_buf_size = 65536,
 
-#define DEST_IP (DEST_IP_1 | (DEST_IP_2 << 8) | (DEST_IP_3 << 16) | (DEST_IP_4 << 24))
+    nat_table_size = 200,
+    nat_port_range_start = 49160,
+    nat_port_range_end = 49180,
+
+    listen_port = 52880,
+
+    gateway_mac_1 = 0x74,
+    gateway_mac_2 = 0xFE,
+    gateway_mac_3 = 0xCE,
+    gateway_mac_4 = 0x8C,
+    gateway_mac_5 = 0x87,
+    gateway_mac_6 = 0x11,
+
+    dest_ip_1 = 146,
+    dest_ip_2 = 190,
+    dest_ip_3 = 62,
+    dest_ip_4 = 39,
+    dest_port = 80
+};
+
+#define DEST_IP (dest_ip_1 | (dest_ip_2 << 8) | (dest_ip_3 << 16) | (dest_ip_4 << 24))
 
 struct int_info {
     int index;
-    uint8_t addr_link[8];
     uint32_t addr_net;
 };
 
@@ -78,9 +94,9 @@ int main() {
 
     ssize_t buf_len;
 
-    buf = (uint8_t *) malloc(BUF_SIZE * sizeof(uint8_t));
-    nat_table = nat_table_alloc(NAT_TABLE_SIZE);
-    port_cntr = NAT_PORT_RANGE_START;
+    buf = (uint8_t *) malloc(packet_buf_size * sizeof(uint8_t));
+    nat_table = nat_table_alloc(nat_table_size);
+    port_cntr = nat_port_range_start;
 
     socket_fd = create_socket(&int_info);
 
@@ -88,31 +104,32 @@ int main() {
     addr.sll_family = AF_PACKET;
     addr.sll_protocol = htons(ETH_P_IP);
     addr.sll_ifindex = int_info.index;
-    addr.sll_halen = sizeof(int_info.addr_link);
-    addr.sll_addr[0] = GATEWAY_MAC_1;
-    addr.sll_addr[1] = GATEWAY_MAC_2;
-    addr.sll_addr[2] = GATEWAY_MAC_3;
-    addr.sll_addr[3] = GATEWAY_MAC_4;
-    addr.sll_addr[4] = GATEWAY_MAC_5;
-    addr.sll_addr[5] = GATEWAY_MAC_6;
+    addr.sll_halen = 6;
+    addr.sll_addr[0] = gateway_mac_1;
+    addr.sll_addr[1] = gateway_mac_2;
+    addr.sll_addr[2] = gateway_mac_3;
+    addr.sll_addr[3] = gateway_mac_4;
+    addr.sll_addr[4] = gateway_mac_5;
+    addr.sll_addr[5] = gateway_mac_6;
 
-    printf("Listening on port %d...\n\n", LISTEN_PORT);
+    LOG_INFO("The proxy is listening on port %d...\n\n", listen_port);
 
-    while ((buf_len = recv(socket_fd, buf, BUF_SIZE, 0)) > 0) {
-        printf("Received packet with length %ld\n", buf_len);
+    while ((buf_len = recv(socket_fd, buf, packet_buf_size, 0)) > 0) {
+        LOG_DEBUG("Packet\n");
+        LOG_DEBUG("|-length %ld\n", buf_len);
 
         if(!handle_packet(buf, nat_table, &int_info, &port_cntr)) {
             continue;
         }
 
-        printf("Sending packet... ");
+        LOG_INFO("Forwarding packet... ");
 
         if(sendto(socket_fd, buf, buf_len, 0, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-            printf("Failed\n\n");
+            LOG_INFO("Failed.\n\n");
             continue;
         }
 
-        printf("Success\n\n");
+        LOG_INFO("Success.\n\n");
     }
 
     close(socket_fd);
@@ -127,7 +144,8 @@ int create_socket(struct int_info *int_info) {
     struct ifreq ifreq;
     struct sockaddr_ll addr;
 
-    uint8_t *ip_splitted;
+    uint8_t addr_link[8];
+    uint8_t *addr_net;
 
     if((socket_fd = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_IP))) < 0) {
         perror("socket()");
@@ -135,7 +153,7 @@ int create_socket(struct int_info *int_info) {
     }
 
     memset(&ifreq, 0, sizeof(struct ifreq));
-    strncpy(ifreq.ifr_name, INT_NAME, IFNAMSIZ - 1);
+    strncpy(ifreq.ifr_name, INTERFACE_NAME, IFNAMSIZ - 1);
 
     if(ioctl(socket_fd, SIOCGIFINDEX, &ifreq) < 0) {
         perror("ioctl(SIOCGIFINDEX)");
@@ -149,7 +167,7 @@ int create_socket(struct int_info *int_info) {
         exit(EXIT_FAILURE);
     }
 
-    memcpy(&int_info->addr_link, &ifreq.ifr_hwaddr.sa_data, 8 * sizeof(uint8_t));
+    memcpy(addr_link, &ifreq.ifr_hwaddr.sa_data, 8 * sizeof(uint8_t));
 
     if(ioctl(socket_fd, SIOCGIFADDR, &ifreq) < 0) {
         perror("ioctl(SIOCGIFADDR)");
@@ -157,8 +175,6 @@ int create_socket(struct int_info *int_info) {
     }
 
     int_info->addr_net = ((struct sockaddr_in *) &ifreq.ifr_addr)->sin_addr.s_addr;
-
-    ip_splitted = (uint8_t *) &int_info->addr_net;
 
     memset(&addr, 0, sizeof(addr));
     addr.sll_family = AF_PACKET;
@@ -170,12 +186,14 @@ int create_socket(struct int_info *int_info) {
         exit(EXIT_FAILURE);
     }
 
-    printf("Socket\n");
-    printf("|-name %s\n", INT_NAME);
-    printf("|-index %d\n", int_info->index);
-    printf("|-mac %x:%x:%x:%x:%x:%x\n", int_info->addr_link[0], int_info->addr_link[1], int_info->addr_link[2], int_info->addr_link[3], int_info->addr_link[4], int_info->addr_link[5]);
-    printf("|-ip %d.%d.%d.%d\n", ip_splitted[0], ip_splitted[1], ip_splitted[2], ip_splitted[3]);
-    printf("Initialized successfully\n\n");
+    addr_net = (uint8_t *) &int_info->addr_net;
+
+    LOG_INFO("Interface\n");
+    LOG_INFO("|-name %s\n", INTERFACE_NAME);
+    LOG_INFO("|-index %d\n", int_info->index);
+    LOG_INFO("|-mac %x:%x:%x:%x:%x:%x\n", addr_link[0], addr_link[1], addr_link[2], addr_link[3], addr_link[4], addr_link[5]);
+    LOG_INFO("|-ip %d.%d.%d.%d\n", addr_net[0], addr_net[1], addr_net[2], addr_net[3]);
+    LOG_INFO("Socket initialized successfully\n\n");
 
     return socket_fd;
 }
@@ -191,41 +209,51 @@ bool handle_packet(uint8_t *buf, struct nat_table *nat_table, struct int_info *i
     struct nat_entry nat_entry_new;
 
     net_len = map_network_header(buf, &net_hdr_map);
-    if(net_len <= 0) {
-        printf("Network header length is not positive. Ignoring packet\n\n");
+    if(net_len == 0) {
+        LOG_DEBUG("The network layer protocol is not recognized.\n\n");
         return false;
     }
 
     trans_len = map_transport_header(buf + net_len, *net_hdr_map.next_proto, &trans_hdr_map);
+
     if(trans_len == 0) {
-        printf("Transport header length is not positive. Ignoring packet\n\n");
+        LOG_DEBUG("The transport layer protocol is not recognized.\n\n");
         return false;
     }
 
     if(*net_hdr_map.addr_dst != int_info->addr_net) {
-        printf("Destination IP is not equal to interface IP. Ignoring packet\n\n");
+        LOG_DEBUG("Destination IP is not equal to the interface IP.\n\n");
         return false;
     }
 
-    if(ntohs(*trans_hdr_map.port_dst) == LISTEN_PORT) {
-        printf("This is a packet from the client\n");
+    if(ntohs(*trans_hdr_map.port_dst) == listen_port) {
+#ifndef ENABLE_DEBUG_LOG
+        LOG_INFO("Packet\n");
+#endif
+        LOG_INFO("|-type: client to server\n");
 
         nat_entry = nat_table_get_by_src(nat_table, *trans_hdr_map.port_src, *net_hdr_map.addr_src);
+
+        LOG_INFO("|-translation table entry: %s\n", nat_entry ? "present" : "new one");
+
         if(!nat_entry) {
             nat_entry_new.port_src = *trans_hdr_map.port_src;
             nat_entry_new.addr_src = *net_hdr_map.addr_src;
             nat_entry_new.port_alloc = get_free_port(nat_table, port_cntr);
             nat_entry_new.alloc_time = time(NULL);
-            // TODO: Add entry removal over time
-
-            printf("NAT entry not found, creating a new one. Allocated port is %u\n", ntohs(nat_entry_new.port_alloc));
+            /* TODO: Add entry removal over time*/
 
             nat_entry = nat_table_insert(nat_table, nat_entry_new);
             if(!nat_entry) {
-                printf("NAT table inser failed. Ignoring packet\n\n");
+                LOG_INFO("NAT table inser failed.\n\n");
                 return false;
             }
         }
+
+        /* TODO: Fill in */
+        /* LOG_INFO("|-src addr: %d.%d.%d.%d\n", );*/
+        LOG_INFO("|-client port: %u\n", ntohs(*trans_hdr_map.port_src));
+        LOG_INFO("|-alloc port: %u\n", ntohs(nat_entry->port_alloc));
 
         *net_hdr_map.checksum = recompute_checksum(*net_hdr_map.checksum, *net_hdr_map.addr_src, int_info->addr_net);
         *net_hdr_map.checksum = recompute_checksum(*net_hdr_map.checksum, *net_hdr_map.addr_dst, DEST_IP);
@@ -234,20 +262,28 @@ bool handle_packet(uint8_t *buf, struct nat_table *nat_table, struct int_info *i
         *trans_hdr_map.checksum = recompute_checksum(*trans_hdr_map.checksum, *net_hdr_map.addr_dst, DEST_IP);
 
         *trans_hdr_map.checksum = recompute_checksum(*trans_hdr_map.checksum, *trans_hdr_map.port_src, nat_entry->port_alloc);
-        *trans_hdr_map.checksum = recompute_checksum(*trans_hdr_map.checksum, *trans_hdr_map.port_dst, htons(DEST_PORT));
+        *trans_hdr_map.checksum = recompute_checksum(*trans_hdr_map.checksum, *trans_hdr_map.port_dst, htons(dest_port));
 
         *net_hdr_map.addr_src = int_info->addr_net;
         *trans_hdr_map.port_src = nat_entry->port_alloc;
         *net_hdr_map.addr_dst = DEST_IP;
-        *trans_hdr_map.port_dst = htons(DEST_PORT);
-    } else if(*trans_hdr_map.port_src == htons(DEST_PORT) && *net_hdr_map.addr_src == DEST_IP) {
-        printf("This is a packet from the server on port %d\n", ntohs(*trans_hdr_map.port_dst));
+        *trans_hdr_map.port_dst = htons(dest_port);
+    } else if(*trans_hdr_map.port_src == htons(dest_port) && *net_hdr_map.addr_src == DEST_IP) {
+#ifndef ENABLE_DEBUG_LOG
+        LOG_INFO("Packet\n");
+#endif
+        LOG_INFO("|-type: server to client\n");
 
         nat_entry = nat_table_get_by_alloc(nat_table, *trans_hdr_map.port_dst);
         if(!nat_entry) {
-            printf("NAT entry not found. Ignoring packet\n\n");
+            LOG_INFO("NAT entry not found.\n\n");
             return false;
         }
+
+        /* TODO: Fill in */
+        /* LOG_INFO("|-src addr: %d.%d.%d.%d\n", );*/
+        LOG_INFO("|-client port: %u\n", ntohs(nat_entry->port_src));
+        LOG_INFO("|-alloc port: %u\n", ntohs(nat_entry->port_alloc));
 
         *net_hdr_map.checksum = recompute_checksum(*net_hdr_map.checksum, *net_hdr_map.addr_src, int_info->addr_net);
         *net_hdr_map.checksum = recompute_checksum(*net_hdr_map.checksum, *net_hdr_map.addr_dst, nat_entry->addr_src);
@@ -255,15 +291,15 @@ bool handle_packet(uint8_t *buf, struct nat_table *nat_table, struct int_info *i
         *trans_hdr_map.checksum = recompute_checksum(*trans_hdr_map.checksum, *net_hdr_map.addr_src, int_info->addr_net);
         *trans_hdr_map.checksum = recompute_checksum(*trans_hdr_map.checksum, *net_hdr_map.addr_dst, nat_entry->addr_src);
 
-        *trans_hdr_map.checksum = recompute_checksum(*trans_hdr_map.checksum, *trans_hdr_map.port_src, htons(LISTEN_PORT));
+        *trans_hdr_map.checksum = recompute_checksum(*trans_hdr_map.checksum, *trans_hdr_map.port_src, htons(listen_port));
         *trans_hdr_map.checksum = recompute_checksum(*trans_hdr_map.checksum, *trans_hdr_map.port_dst, nat_entry->port_src);
 
         *net_hdr_map.addr_src = int_info->addr_net;
-        *trans_hdr_map.port_src = htons(LISTEN_PORT);
+        *trans_hdr_map.port_src = htons(listen_port);
         *net_hdr_map.addr_dst = nat_entry->addr_src;
         *trans_hdr_map.port_dst = nat_entry->port_src;
     } else {
-        printf("This packet is not intended for proxy. Ignoring packet\n\n");
+        LOG_DEBUG("Packet is not intended for proxy.\n\n");
         return false;
     }
 
@@ -315,14 +351,14 @@ uint8_t map_transport_header(uint8_t *buf, uint8_t proto, struct trans_hdr_map *
 uint16_t get_free_port(struct nat_table *nat_table, uint16_t *port_cntr) {
     (*port_cntr)++;
 
-    if(*port_cntr < NAT_PORT_RANGE_START || *port_cntr > NAT_PORT_RANGE_END) {
-        *port_cntr = NAT_PORT_RANGE_START;
+    if(*port_cntr < nat_port_range_start || *port_cntr > nat_port_range_end) {
+        *port_cntr = nat_port_range_start;
     }
 
     while(nat_table_get_by_alloc(nat_table, *port_cntr)) {
         (*port_cntr)++;
-        if(*port_cntr > NAT_PORT_RANGE_END) {
-            *port_cntr = NAT_PORT_RANGE_START;
+        if(*port_cntr > nat_port_range_end) {
+            *port_cntr = nat_port_range_start;
         }
     }
 
