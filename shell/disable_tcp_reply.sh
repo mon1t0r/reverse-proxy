fdafaba01030fd1283225836f38c4dce60132b51
mon1t0r <westxlebyshek@gmail.com>
2025-04-19 23:12:56 +0200

    Shell script improvements

--- shell/disable_tcp_reply.sh
+++ shell/disable_tcp_reply.sh
@@ -1,2 +1,10 @@
-iptables -A INPUT -p tcp --destination-port 52880 -j DROP
-iptables -A INPUT -p tcp --destination-port 49160:49192 -j DROP
+#!/bin/sh
+
+LISTEN_PORT=52880
+NAT_PORT_RANGE=49160:49192
+
+set -x
+
+iptables -A INPUT -p tcp --destination-port $LISTEN_PORT -j DROP
+iptables -A INPUT -p tcp --destination-port $NAT_PORT_RANGE -j DROP
+
--- shell/enable_tcp_reply.sh
+++ shell/enable_tcp_reply.sh
@@ -1,3 +1,10 @@
-iptables -D INPUT -p tcp --destination-port 52880 -j DROP
-iptables -D INPUT -p tcp --destination-port 49160:49192 -j DROP
+#!/bin/sh
+
+LISTEN_PORT=52880
+NAT_PORT_RANGE=49160:49192
+
+set -x
+
+iptables -D INPUT -p tcp --destination-port $LISTEN_PORT -j DROP
+iptables -D INPUT -p tcp --destination-port $NAT_PORT_RANGE -j DROP
 
